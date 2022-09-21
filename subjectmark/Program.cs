35973b2710a0c00943c0bccb731f30d75a285862
darshanthorat99 <113414840+darshanthorat99@users.noreply.github.com>
2022-09-21 10:33:07 +0530

    Add project files.

---
+++ subjectmark/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subjectmark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the mark of physics");
+            double n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the mark of math");
+            double n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the mark of chemisty");
+            double n3 = Convert.ToInt32(Console.ReadLine());
+            double per = ((n1 + n2 + n3) / 300) * 100;
+            Console.WriteLine($"percentage is {per}%");
+            if (per >= 80)
+            {
+                Console.WriteLine(" grade is A");
+            }
+            else if (per >= 70 && per < 80)
+            {
+                Console.WriteLine("grade is B");
+            }
+            else if (per >= 60 && per < 70)
+            {
+                Console.WriteLine("grade is c");
+            }
+            else if (per < 60)
+            {
+                Console.WriteLine("grade is d");
+            }
+        }
+    }
+}
---
+++ subjectmark/salary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subjectmark
+{
+    internal class salary
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter  the basic salary");
+            double bs=Convert.ToDouble(Console.ReadLine());
+            if(bs<10000)
+            {
+                double da = bs * 0.8;
+                double hra = bs * 0.2;
+                double gross = bs + da + hra;
+                Console.WriteLine($"the gross salary is {gross}");
+            }
+            if (bs >10000 && bs<20000)
+            {
+                double da = bs * 0.9;
+                double hra = bs * 0.35;
+                double gross = bs + da + hra;
+                Console.WriteLine($"the gross salary is {gross}");
+            }
+            else
+            {
+                double da = bs * 0.95;
+                double hra = bs * 0.4;
+                double gross = bs + da + hra;
+                Console.WriteLine($"the gross salary is {gross}");
+            }
+        }
+    }
+}
+ 
